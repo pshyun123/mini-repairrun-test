49d786f159775fb3fb77a291f517abd2b6a82eb6
hjck4433 <hjck4433@gmail.com>
2023-11-02 19:11:37 +0900

    test react file

--- src/App.js
+++ src/App.js
@@ -1,24 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import Test from "./pages/Test";
+import { createGlobalStyle } from "styled-components";
+import reset from "styled-reset";
+
+const GlobalStyle = createGlobalStyle`
+  ${reset}
+  * {
+    box-sizing: border-box;
+  }
+`;
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <GlobalStyle />
+      <Test />
+    </>
   );
 }
 
---
+++ src/pages/Test.jsx
@@ -0,0 +1,13 @@
+const Test = () => {
+    return (
+        <div>
+            <p>
+                테스트페이지 입니다.
+                <ul>
+                    <li>아아!</li>
+                </ul>
+            </p>
+        </div>
+    )
+};
+export default Test;
